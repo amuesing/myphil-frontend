3a1004caf4b3e1b0d684082eb208c2332d51f385
“Aaron <github email address>
2021-04-09 10:56:47 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './Components/Header/Header.js'
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
